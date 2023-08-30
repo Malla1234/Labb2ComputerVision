4ef6fee7d40b7f828abe743aa3b318084fe963c7
Malin Lindblom <malin.lindblom@edu.edugrade.se>
2023-08-30 11:45:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace labb2comp
+{
+    class Program
+    {
+
+        // Add your Computer Vision key and endpoint
+        static string key = "58999e27ecfd49cca2fb62fe3b260bc3";
+        static string endpoint = "https://cogazureeuropawest.cognitiveservices.azure.com/";
+
+        static async Task Main(string[] args)
+        {
+
+            // Create a client
+            ComputerVisionClient client = Authenticate(endpoint, key);
+
+            // Prompt the user to enter an image URL
+            Console.Write("Enter the URL of the image: ");
+            string imageUrl = Console.ReadLine();
+
+            // Analyze the user-provided image URL
+            await AnalyzeImageUrl(client, imageUrl);
+        }
+
+        /*
+         * AUTHENTICATE
+         * Creates a Computer Vision client used by each example.
+         */
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Tags,
+                VisualFeatureTypes.Description
+            };
+
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}...");
+            Console.WriteLine();
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
+
+            // Image tags and their confidence score
+            Console.WriteLine("Tags:");
+            foreach (var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+
+            // Image captions
+            Console.WriteLine("\nCaptions:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} (Confidence: {caption.Confidence})");
+            }
+
+            // Output current directory for debugging
+            Console.WriteLine($"Current Directory: {Environment.CurrentDirectory}");
+
+            // Generate and display thumbnail
+            Console.WriteLine("\nGenerating Thumbnail...");
+            var thumbnailStream = await client.GenerateThumbnailAsync(50, 50, imageUrl, true);
+            using (var thumbnailImage = System.Drawing.Image.FromStream(thumbnailStream))
+            {
+                thumbnailImage.Save("thumbnail.png");
+            }
+
+            Console.WriteLine("Thumbnail image saved as 'thumbnail.png'");
+
+            Console.WriteLine();
+        }
+    }
+}
